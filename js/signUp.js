79a5525812a948161a3113a1796c964bdbb3ca4a
burkoo <hbakin98@gmail.com>
2021-01-10 14:14:44 +0300

    background , login , signup

--- js/signUp.js
+++ js/signUp.js
@@ -1,8 +1,18 @@
+var userType = 's';
+
+
 function setType(type){
     if(type == 'employer'){
         document.getElementById("chb_student").checked = false
+        userType = 'e'
     }
     if(type == 'student'){
         document.getElementById("chb_employer").checked = false
+        userType = 's'
     }
+}
+
+function signUp(){
+
+    alert("this");
 }
