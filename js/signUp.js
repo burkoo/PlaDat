9da2b2fe6767d0f994d0d2667992e1e54eceb141
burkoo <hbakin98@gmail.com>
2021-01-10 15:56:05 +0300

    signup successfull

--- js/signUp.js
+++ js/signUp.js
@@ -1,18 +1,69 @@
-var userType = 's';
-
-
-function setType(type){
-    if(type == 'employer'){
-        document.getElementById("chb_student").checked = false
-        userType = 'e'
-    }
-    if(type == 'student'){
-        document.getElementById("chb_employer").checked = false
-        userType = 's'
-    }
-}
+var baseURL = "http://127.0.0.1:9090";
 
 function signUp(){
 
-    alert("this");
+    var userType = document.getElementById("userTypeInput").value;
+    var fullname = document.getElementById("fullnameInput").value;
+    var email = document.getElementById("emailInput").value;
+    var passwd = document.getElementById("passwdInput").value;
+    var cpasswd = document.getElementById("confirmPasswdInput").value;
+
+
+    if(fullname == ""){
+        alert("Fullname is required !");
+    } else if(email == ""){
+        alert("E-mail is required !");
+    } else if(passwd == ""){
+        alert("Password is required !");
+    } else if(cpasswd == ""){
+        alert("Confirm Password is required !");
+    } 
+    // inputs are are all filled
+    else {
+        if(passwd != cpasswd){
+            alert("Passwords don't match !");
+        } else {
+            // msg = userType + fullname + email + passwd + cpasswd;
+            // alert(msg);
+            // send signup request
+
+            var finalURL = baseURL + "/ssignup?" + "name=" + fullname + "&email=" + email + "&pw=" + passwd;
+
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("POST",finalURL,true);
+            xmlhttp.onreadystatechange = function() {
+                if(xmlhttp.readyState ===4 && xmlhttp.status ===200){
+                    console.log("success");
+                    showModalSignUpSuccess();
+                }
+            };
+            xmlhttp.send();
+        }
+    }   
+    
 }
+
+function showModalSignUpSuccess(){
+    // set innerHtml of modal
+    document.getElementById("successModal").innerHTML =
+        '<div class="modal-dialog"><div class="modal-content">'+
+        '<div class="modal-header">' +
+        '<h5 class="modal-title" id="exampleModalLabel">Welcome To PlaDat</h5>' +
+        '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>' +
+        '</div>' +
+        '<div class="modal-body">'+
+
+
+
+            '<p>Account created succesfully.</p>'+
+       
+       
+        '</div>'+
+        '<div class="modal-footer">'+
+        '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="window.location.href=\'./login.html\';">Okay</button>'+
+        '</div>'+
+        '</div>'+
+        '</div>';
+    $('#successModal').modal('show')
+
+};
