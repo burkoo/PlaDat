fd8d6e23b72e3a11bda0ad3d3a4db1486b50ccf6
burkoo <hbakin98@gmail.com>
2021-01-15 01:44:28 +0300

    successfull job application

--- js/searchJobs.js
+++ js/searchJobs.js
@@ -1,7 +1,12 @@
 var skillTableCounter = 0;
 var baseURL = "http://127.0.0.1:9090";
 
+var jobsTableCounter = 0;
+var offersTableCounter = 0;
+
 var searchClicked = false;
+
+var job_ids = [];
 
 function getUserId(){
     const USER_ID = sessionStorage.getItem('userID');
@@ -18,14 +23,7 @@ function showModal(){
         '</div>' +
         '<div class="modal-body">'+
 
-
-
             '<p>Modal body text goes here.</p>'+
-       
-       
-       
-       
-       
        
         '</div>'+
         '<div class="modal-footer">'+
@@ -35,6 +33,66 @@ function showModal(){
         '</div>'+
         '</div>';
     $('#exampleModal').modal('show')
+    
+};
+
+function showJobDetailsModal(jobId){
+
+    var job_desc;
+    var job_location;
+    var job_emp_type;
+    var company_name;
+    var country_name;
+    var skill_list;
+
+    var finalURL = baseURL + "/job_details?" + "job_id=" + jobId;
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET",finalURL,false);
+    xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState ===4 && xmlhttp.status ===200){
+            var response = JSON.parse(xmlhttp.responseText);
+            job_desc = response.job_desc
+            job_location = response.city;
+            job_emp_type = response.emp_type;
+            company_name = response.company_name;
+            country_name = response.country;
+            skill_list = response.skill_list;
+        }
+    };
+    xmlhttp.send();
+
+    var body = '<h5>Company Name</h5>'
+    body += '<p>' + company_name + '</p>';
+    body += '<h5>Job Description</h5>'
+    body += '<p>' + job_desc + '</p>';
+    body += '<h5>Job Location</h5>'
+    body += '<p>' + job_location + ', ' + country_name + '</p>';
+    body += '<h5>Employment Type</h5>'
+    body += '<p>' + job_emp_type + '</p>';
+    body += '<h5>Requirements</h5>'
+    skill_list.forEach(element => {
+        body += '<p><b>' + element.name + '</b></p>';
+        body += '<p>' + element.desc + '</p>';
+    });
+
+    document.getElementById("jobDetailsModal").innerHTML =
+        '<div class="modal-dialog modal-lg"><div class="modal-content">'+
+        '<div class="modal-header">' +
+        '<h5 class="modal-title" id="exampleModalLabel">Job Details</h5>' +
+        '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>' +
+        '</div>' +
+        '<div class="modal-body">'+
+
+            body +
+       
+        '</div>'+
+        '<div class="modal-footer">'+
+        '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>'+
+        '</div>'+
+        '</div>'+
+        '</div>';
+    $('#jobDetailsModal').modal('show')
     
 };
 
@@ -48,14 +106,7 @@ function showModalOffer(){
         '</div>' +
         '<div class="modal-body">'+
 
-
-
             '<p>Modal body text goes here.</p>'+
-       
-       
-       
-       
-       
        
         '</div>'+
         '<div class="modal-footer">'+
@@ -102,12 +153,6 @@ function showSkillsModal(){
                         skillTableRow += '</td><td><input name="skill" value="';
                         skillTableRow += skill_id.toString();
                         skillTableRow += '" type="checkbox" class="form-check-input"></td></tr>';
-                        // <tr>
-                        //     <th scope="row">1</th>
-                        //     <td>Java</td>
-                        //     <td><input type="checkbox" class="form-check-input"></td>
-                        // </tr>
-    
                         document.getElementById("skillsTableBody").insertAdjacentHTML('beforeend', skillTableRow);
                         
                     }
@@ -131,4 +176,94 @@ function getSelectedCheckboxValues(name) {
     });
     return values;
 }
+
+function searchJobs(){
+
+    var skillsToUse = getSelectedCheckboxValues('skill');
+
+    var finalURL = baseURL + "/search_jobs_by_skill_ids?" + "term=" + skillsToUse.toString();
+        
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET",finalURL,false);
+        xmlhttp.onreadystatechange = function() {
+            if(xmlhttp.readyState ===4 && xmlhttp.status ===200){
+                var response = JSON.parse(xmlhttp.responseText);
+    
+                if(response.jobs != null){
+                    for (let index = 0; index < response.jobs.length; index++) {
+
+                        var city = response.jobs[index].city;
+                        var country = response.jobs[index].country;
+                        var companyName = response.jobs[index].company_name;
+                        var empType = response.jobs[index].emp_type;
+                        var jobDesc = response.jobs[index].job_desc;
+                        var jobId = response.jobs[index].id;
+
+                        if(!job_ids.includes(jobId)){
+                            jobsTableCounter += 1;
+
+                            var jobsTableRow;
+                            jobsTableRow = '<tr><th scope="row">';
+                            jobsTableRow += jobsTableCounter.toString();
+                            jobsTableRow += '</th><td>';
+                            jobsTableRow += jobDesc;
+                            jobsTableRow += '</td><td>';
+                            jobsTableRow += companyName;
+                            jobsTableRow += '</td><td>';
+                            jobsTableRow += city + ', ' + country;
+                            jobsTableRow += '</td><td><button onclick="showJobDetailsModal(' + jobId + ');" class="btn btn-info">Details</button>';
+                            jobsTableRow += '<button style="margin-left: 5px;"class="btn btn-success" onclick="applyJob('+ jobId +',\'' + jobDesc +'\');">Apply</button></td></tr>';
+
+                            document.getElementById("jobsTableBody").insertAdjacentHTML('beforeend', jobsTableRow);
+
+                            job_ids.push(jobId);
+                        }
+    
+                    }
+                }
+            }
+        };
+        xmlhttp.send();
+
+}
+
+function applyJob(job_id,job_desc){
+
+    var stu_id = getUserId();
+
+    var finalURL = baseURL + "/add_application?" + "job_id=" + job_id + "&stu_id=" + stu_id + "&direction=" + "false"; // false for student apply
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST",finalURL,false);
+    xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState ===4 && xmlhttp.status ===200){
+            var response = JSON.parse(xmlhttp.responseText);
+            alert(job_desc);
+            showAppliedModal(job_desc);
+        }
+    };
+    xmlhttp.send();
+}
+
+function showAppliedModal(job_desc){
+    // set innerHtml of modal
+    document.getElementById("jobApplicationModal").innerHTML =
+        '<div class="modal-dialog"><div class="modal-content">'+
+        '<div class="modal-header">' +
+        '<h5 class="modal-title" id="exampleModalLabel">Job Application</h5>' +
+        '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>' +
+        '</div>' +
+        '<div class="modal-body">'+
+
+            '<p>Applied to "' + job_desc + '" successfuly.</p>'+
+       
+        '</div>'+
+        '<div class="modal-footer">'+
+        '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>'+
+        '</div>'+
+        '</div>'+
+        '</div>';
+    $('#jobApplicationModal').modal('show')
+    
+};
 
--- js/searchStudents.js
+++ js/searchStudents.js
@@ -1,21 +1,84 @@
-function showModal(){
+var jobsTableCounter = 0;
+var baseURL = "http://127.0.0.1:9090";
+
+var applicantsTableCounter = 0;
+
+var searchClicked = false;
+
+var stu_ids = [];
+
+function getUserId(){
+    const USER_ID = sessionStorage.getItem('userID');
+    return USER_ID;
+}
+
+function showStudentDetailsModal(stu_id){
+
+    var age;
+    var city;
+    var department;
+    var emp_pref;
+    var faculty;
+    var grade;
+    var name;
+    var skills;
+    var university;
+
+    var finalURL = baseURL + "/stu_detail?" + "stu_id=" + stu_id;
+        
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET",finalURL,false);
+    xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState ===4 && xmlhttp.status ===200){
+            var response = JSON.parse(xmlhttp.responseText);
+            age = response.age
+            city = response.city;
+            department = response.department;
+            emp_pref = response.emp_pref;
+            faculty = response.faculty;
+            grade = response.faculty;
+            name = response.name;
+            skills = response.skills;
+            university = response.university;
+        }
+    };
+    xmlhttp.send();
+
+
+    var body = '<h5>Name</h5>'
+    body += '<p>' + name + '</p>';
+    body += '<h5>Age</h5>'
+    body += '<p>' + age + '</p>';
+    body += '<h5>University</h5>'
+    body += '<p>' + university + '</p>';
+    body += '<h5>Department</h5>'
+    body += '<p>' + department + '</p>';
+    body += '<h5>Employment Type</h5>'
+    body += '<p>' + emp_pref + '</p>';
+    body += '<h5>Grade</h5>'
+    body += '<p>' + grade + '</p>';
+    body += '<h5>Skills</h5>'
+    skills.forEach(element => {
+        var splitted_text = element.split(":");
+
+        var skill_id = splitted_text[0];
+        var skillName = splitted_text[1];
+        var skillDesc = splitted_text[2];
+        body += '<p><b>' + skillName + '</b></p>';
+        body += '<p>' + skillDesc + '</p>';
+    });
+
+
     // set innerHtml of modal
-    document.getElementById("exampleModal").innerHTML =
-        '<div class="modal-dialog"><div class="modal-content">'+
+    document.getElementById("studentDetailModal").innerHTML =
+        '<div class="modal-dialog modal-lg"><div class="modal-content">'+
         '<div class="modal-header">' +
-        '<h5 class="modal-title" id="exampleModalLabel">Java Developer in A Company</h5>' +
+        '<h5 class="modal-title" id="exampleModalLabel">Student Details</h5>' +
         '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>' +
         '</div>' +
         '<div class="modal-body">'+
 
-
-
-            '<p>Modal body text goes here.</p>'+
-       
-       
-       
-       
-       
+            body+
        
         '</div>'+
         '<div class="modal-footer">'+
@@ -24,6 +87,83 @@ function showModal(){
         '</div>'+
         '</div>'+
         '</div>';
-    $('#exampleModal').modal('show')
+    $('#studentDetailModal').modal('show')
     
 };
+
+function showJobsModal(){
+
+    if(searchClicked == false){
+        var userId = getUserId();
+    
+        var finalURL = baseURL + "/company_detail?" + "company_id=" + userId;
+        
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET",finalURL,false);
+        xmlhttp.onreadystatechange = function() {
+            if(xmlhttp.readyState ===4 && xmlhttp.status ===200){
+                var response = JSON.parse(xmlhttp.responseText);
+    
+                if(response.job_lis != null){
+                    for (let index = 0; index < response.job_lis.length; index++) {
+    
+                        var job_id = response.job_lis[index];
+
+                        var job_desc = getJobDetail(job_id);
+    
+                        jobsTableCounter += 1;
+    
+                        var jobsTableRow;
+                        jobsTableRow = '<tr><th scope="row">';
+                        jobsTableRow += jobsTableCounter.toString();
+                        jobsTableRow += '</th><td>';
+                        jobsTableRow += job_desc;
+                        jobsTableRow += '</td><td><input name="job" value="';
+                        jobsTableRow += job_id.toString();
+                        jobsTableRow += '" type="checkbox" class="form-check-input"></td></tr>';
+                        document.getElementById("jobsTableBody").insertAdjacentHTML('beforeend', jobsTableRow);
+                        
+                    }
+                }
+            }
+        };
+        xmlhttp.send();
+        searchClicked = true;
+    }
+
+    $('#jobsModal').modal('show')
+
+}
+
+function getJobDetail(job_id){
+
+    var job_desc;
+    var finalURL = baseURL + "/job_details?" + "job_id=" + job_id;
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET",finalURL,false);
+    xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState ===4 && xmlhttp.status ===200){
+            var response = JSON.parse(xmlhttp.responseText);
+            job_desc = response.job_desc
+        }
+    };
+    xmlhttp.send();
+
+    return job_desc;
+}
+
+function getSelectedCheckboxValues(name) {
+    const checkboxes = document.querySelectorAll(`input[name="${name}"]:checked`);
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+        values.push(checkbox.value);
+    });
+    return values;
+}
+
+function searchJobs(){
+
+    var jobsToUse = getSelectedCheckboxValues('job');
+    alert(jobsToUse.toString());
+}
